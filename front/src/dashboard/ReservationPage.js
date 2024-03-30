5cd7812a59952e35141176495f69ed4a94813751
Louie T <tselouie@gmail.com>
2024-03-30 19:02:11 -0400

    init commit

---
+++ front/src/dashboard/ReservationPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ReservationPage() {
+  return (
+    <div>
+      <h1>This is an empty page</h1>
+    </div>
+  );
+}
---
+++ front/src/dashboard/UsersPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function UsersPage() {
+  return (
+    <div>
+      <h1>This is an empty users page</h1>
+    </div>
+  );
+}
